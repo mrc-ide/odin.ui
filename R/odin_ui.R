5d64a879fab5c0a03529c90185cecca30526f0e8
Rich FitzJohn <rich.fitzjohn@gmail.com>
2018-05-02 08:50:08 +0100

    Steps towards a dynamic interface

--- R/odin_ui.R
+++ R/odin_ui.R
@@ -1,38 +1,76 @@
 ##' Odin shiny app
 ##' @title Odin Shiny Application
+##' @param model An odin model
+##' @param default_time Default time
 ##' @export
-odin_app <- function() {
+odin_app <- function(model, default_time) {
   shiny::shinyApp(
-    ui = odin_ui(),
+    ui = odin_ui(model, default_time),
     server = odin_server)
 }
 
 
-odin_server <- function(input, output) {
+odin_server <- function(input, output, session) {
+  browser()
   output$result_plot <- shiny::renderPlot({
-    x <- stats::rnorm(input$n)
-    y <- stats::rnorm(input$n)
+    x <- stats::rnorm(100)
+    y <- stats::rnorm(100)
     graphics::plot(x, y, asp = 1)
   })
 }
 
 
-odin_ui <- function() {
+odin_ui <- function(model, default_time) {
   shiny::shinyUI(shiny::fluidPage(
     shiny::titlePanel("odin ui"),
 
     shiny::sidebarLayout(
-      shiny::sidebarPanel(
-        shiny::sliderInput("n",
-                           "Number of points:",
-                           min = 1,
-                           max = 1000,
-                           value = 500)
-      ),
-
+      odin_ui_sidebar(model, default_time),
       shiny::mainPanel(
         shiny::plotOutput("result_plot")
       )
     )
   ))
+}
+
+
+odin_ui_sidebar <- function(model, default_time) {
+  els <- c(odin_ui_parameters(model),
+           odin_ui_time(default_time))
+  do.call(shiny::sidebarPanel, unname(els), quote = TRUE)
+}
+
+
+odin_ui_parameters <- function(model) {
+  x <- stats::coef(model)
+
+  if (!all(x$model$rank)) {
+    stop("Only scalar inputs are currently supported")
+  }
+  if (!all(x$model$has_default)) {
+    stop("All inputs must have defaults")
+  }
+
+  ## TODO: can we have a real list structure here?
+  if (nrow(x) > 0L) {
+    pars <- c(list(shiny::h2("Parameters")),
+              unname(Map(shiny::numericInput,
+                         paste0("pars_", x$name), x$name, x$default_value)))
+  } else {
+    pars <- NULL
+  }
+
+  pars
+}
+
+
+odin_ui_time <- function(default_time) {
+  if (length(default_time) == 1L) {
+    default_time <- c(0, default_time)
+  } else {
+    ## assume default_time is a 1 or 2 element numeric
+  }
+  list(shiny::h2("Time"),
+       shiny::numericInput("time_start", "start", default_time[[1L]]),
+       shiny::numericInput("time_end", "start", default_time[[2L]]))
 }
--- tests/testthat/test-odin-ui.R
+++ tests/testthat/test-odin-ui.R
@@ -1,6 +1,18 @@
 context("odin.ui")
 
 
-test_that("dummy test", {
-  expect_is(odin_ui(), "shiny.tag.list")
+test_that("parameter inteface generates as expected", {
+  gen <- odin::odin({
+    deriv(N) <- r * N * (1 - N / K)
+    initial(N) <- N0
+    N0 <- user(1)
+    K <- user(100)
+    r <- user(0.5)
+  })
+
+  p <- odin_ui_parameters(gen)
+  expect_is(p, "list")
+  expect_equal(length(p), 4L)
+  id <- vapply(p[-1], function(x) x$children[[2]]$attribs$id, "")
+  expect_equal(id, paste0("pars_", coef(gen)$name))
 })
