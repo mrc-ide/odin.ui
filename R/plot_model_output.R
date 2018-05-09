54745decae70d41c4b46db14fe575d193ffeea75
Thibaut Jombart <thibautjombart@gmail.com>
2018-05-09 16:53:10 +0100

    final tweaks for first go

--- R/plot_model_output.R
+++ R/plot_model_output.R
@@ -17,14 +17,18 @@ plot_model_output <- function(xy, include, cols) {
 
   ## Generation of the basic plot
   var_to_keep <- names(include)[include]
-  df <- cbind.data.frame(time = xy[, 1],
+  time <- round_time(xy[, 1, drop = TRUE])
+  df <- cbind.data.frame(time = time,
                          xy[, var_to_keep, drop = FALSE])
   out <- dygraphs::dygraph(df)
 
 
   ## Customisation of the plot
 
-  out <- dygraphs::dyOptions(out, colors = unname(cols[var_to_keep]))
+  out <- dygraphs::dyOptions(out, colors = unname(cols[var_to_keep]),
+                             labelsKMB = TRUE,
+                             digitsAfterDecimal = 0,
+                             animatedZooms = TRUE)
   out <- dygraphs::dyAxis(out, "x", label = "Time", drawGrid = FALSE)
   out <- dygraphs::dyAxis(out, "y", label = NULL, drawGrid = FALSE)
   out <- dygraphs::dyRangeSelector(out, strokeColor = "#00000099",
@@ -37,6 +41,10 @@ plot_model_output <- function(xy, include, cols) {
                                highlightSeriesBackgroundAlpha = 1,
                                hideOnMouseOut = TRUE)
 
+  out <- dygraphs::dyLegend(out, show = "follow",
+                            showZeroValues = TRUE,
+                            labelsSeparateLines = TRUE,
+                            hideOnMouseOut = TRUE)
   out <- dygraphs::dyCSS(out, system.file("dygraphs.css", package = "odin.ui"))
   out
 }
