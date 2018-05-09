dc16551c10bb9f6161e73e7d3d954ff07acb7bb4
Thibaut Jombart <thibautjombart@gmail.com>
2018-05-09 13:45:42 +0100

    adding separate file for plotting

---
+++ R/plot_model_output.R
@@ -0,0 +1,24 @@
+
+## Internal function generating the plotting output.
+
+
+plot_model_output <- function(xy, include, cols) {
+
+  dygraphs::
+
+  op <- graphics::par(mar = c(4.6, 4.6, .1, .1))
+  on.exit(graphics::par(op))
+
+  x <- xy[, 1, drop = TRUE]
+
+  if (any(include)) {
+    y <- xy[, names(include)[include], drop = FALSE]
+    graphics::matplot(x, y, type = "l", lty = 1, las = 1, col = cols[include],
+                      xlab = "Time", ylab = "Variable")
+  } else {
+    graphics::plot(x, x, type = "n", las = 1, yaxt = "n",
+                   xlab = "Time", ylab = "Variable")
+    pos <- mean(range(x))
+    graphics::text(pos, pos, "(nothing to plot)")
+  }
+}
