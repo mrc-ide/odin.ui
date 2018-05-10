85a4a7cdd6a9e61b8ea05ed1104814f5c2552eab
Thibaut Jombart <thibautjombart@gmail.com>
2018-05-10 15:52:56 +0100

    adding 2 palettes

---
+++ R/palettes.R
@@ -0,0 +1,16 @@
+## This color palettes will be used by default for plotting outputs. They take a
+## single argument 'n' which is the number of colors to be taken from the
+## palette.
+
+palette1 <- grDevices::colorRampPalette(
+  c("#CD88AF", "#9E4779", "#711A4B", "#440027", "#FFE4AA", "#C6A359", 
+    "#8E6A21", "#553A00", "#7986AC", "#445585", "#1E2F5F", "#061439", 
+    "#D4EE9F", "#97B953", "#62841F", "#354F00")
+)
+
+
+palette2 <- grDevices::colorRampPalette(
+  c("#7BB992", "#418F5E", "#176735", "#003E17", "#FFF3AA", "#C6B759", "#8E7E21",
+    "#554900", "#FFB9AA", "#C66C59", "#8E3421", "#550F00", "#8B7AAE", "#5A4587",
+    "#331E60", "#16063A")
+)
