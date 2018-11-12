32429103099da7f4bdddb095bfe18502b4239cae
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-11-12 15:38:15 +0000

    Start working on the teaching app

---
+++ R/app-explore.R
@@ -0,0 +1,101 @@
+##' Create an odin app for exploring a model
+##' @title Create odin app for exploring a model
+##'
+##' @param path_config Path to YAML configuration
+##'
+##' @param env Environment to evaluate any "extra" functions in.  If
+##'   these functions are pure (and they should be!) this can be
+##'   safely ignored.
+##'
+##' @export
+odin_ui_explore <- function(path_config, env = .GlobalEnv) {
+  config <- odin_ui_explore_config(path_config, env)
+  shiny::shinyApp(odin_ui_explore_ui(config), odin_ui_explore_server(config))
+}
+
+
+odin_ui_explore_ui <- function(config) {
+  code <- shiny::tagList(
+    shiny::h2("Code"),
+    shinyAce::aceEditor("code", mode = "r", readOnly = TRUE,
+                        value = odin.ui:::read_text(config$code)))
+  docs <- shiny::includeMarkdown(config$docs)
+  title <- config$title
+  ui <- shiny::shinyUI(
+    shiny::navbarPage(
+      title,
+      id = "odin_ui_navbar",
+      inverse = TRUE,
+      shiny::tabPanel(
+        "Code & documentation",
+        shiny::fluidRow(
+          shiny::column(6, code),
+          shiny::column(6, docs))),
+      shiny::tabPanel(
+        "Run",
+        odin.ui:::mod_model_ui("model", NULL)),
+      shiny::tabPanel(
+        "Parameters",
+        odin.ui:::mod_parameter_input("odin_parameter", NULL))))
+}
+
+
+odin_ui_explore_server <- function(config) {
+  model <- odin::odin_(config$code)
+  default_time <- config$default_time
+  parameters <- config$parameters
+  extra <- config$extra
+
+  function(input, output, session) {
+    shiny::callModule(mod_model_server, "model",
+                      model, default_time, parameters, extra)
+    shiny::callModule(mod_parameter_server, "odin_parameter",
+                      model, default_time, parameters, extra)
+  }
+}
+
+
+odin_ui_explore_config <- function(path_config, env) {
+  config <- yaml::yaml.load_file(path_config)
+
+  required <- "default_time"
+  optional <- c("title", "code", "docs", "parameters", "extra")
+  assert_has_fields(config, required, optional, basename(path_config))
+
+  base <- tools::file_path_sans_ext(basename(path_config))
+  wd <- normalizePath(dirname(path_config), mustWork = TRUE)
+
+  f <- function(field) {
+    sprintf("%s:%s", basename(path_config), field)
+  }
+
+  assert_scalar_numeric(config$default_time, f("default_time"))
+
+  if (is.null(config$title)) {
+    config$title <- base
+  } else {
+    assert_scalar_character(config$title, f("title"))
+  }
+
+  if (is.null(config$code)) {
+    config$code <- sprintf("%s.R", base)
+  }
+  assert_file_exists(config$code, f("code"), wd = wd)
+  ## TODO: this is not great with absolute paths!
+  config$code <- file.path(wd, config$code)
+
+  if (is.null(config$docs)) {
+    config$docs <- sprintf("%s.md", base)
+  }
+  assert_file_exists(config$docs, f("docs"), wd = wd)
+  ## TODO: this is not great with absolute paths!
+  config$docs <- file.path(wd, config$docs)
+
+  if (!is.null(config$extra)) {
+    assert_named(config$extra, TRUE, f("extra"))
+    config$extra <- lapply(config$extra, function(def)
+      eval(parse(text = def), env))
+  }
+
+  config
+}
