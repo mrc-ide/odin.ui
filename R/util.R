e61311bb21f3593875c17b5c8f6874df17619a3f
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-11-12 16:03:06 +0000

    More defensive yaml reading

--- R/util.R
+++ R/util.R
@@ -82,3 +82,25 @@ odin_footer <- function() {
 odin_css <- function() {
   shiny::includeCSS(odin_ui_file("css/styles.css"))
 }
+
+
+yaml_load <- function(string) {
+  ## More restrictive true/false handling.  Only accept if it maps to
+  ## full (true|yes) / (false|no):
+  handlers <- list(
+    "bool#yes" = function(x)
+      if (tolower(x) %in% c("true", "yes")) TRUE else x,
+    "bool#no" = function(x)
+      if (tolower(x) %in% c("false", "no")) FALSE else x)
+  yaml::yaml.load(string, handlers = handlers)
+}
+
+
+yaml_read <- function(filename) {
+  catch_yaml <- function(e) {
+    stop(sprintf("while reading '%s'\n%s", filename, e$message),
+         call. = FALSE)
+  }
+  tryCatch(yaml_load(read_text(filename)),
+           error = catch_yaml)
+}
