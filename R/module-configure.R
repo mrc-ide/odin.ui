efc62fa9bf6eef0f8172d7acf1d1f572f06f50c4
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-05-20 17:36:19 +0100

    Modularise the configure code

---
+++ R/module-configure.R
@@ -0,0 +1,27 @@
+mod_configure_ui <- function(id) {
+  ns <- shiny::NS(id)
+  shiny::tagList(
+    shiny::titlePanel("Configure"),
+    shiny::h3("Data"),
+    shiny::textOutput(ns("data_status")),
+    shiny::selectInput(ns("data_time_variable"),
+                       "Select time variable",
+                       character(0)))
+}
+
+
+mod_configure_server <- function(input, output, session, data, model) {
+  output$data_status <- shiny::renderText({
+    if (is.null(data())) {
+      msg <- "Please upload data"
+    } else {
+      msg <- sprintf("%d rows of data have been uploaded", nrow(data()))
+      vars <- names(data())
+      prev <- input$data_time_variable
+      selected <- if (!is.null(prev) && prev %in% vars) prev else NULL
+      shiny::updateSelectInput(session, "data_time_variable",
+                               choices = vars, selected = selected)
+    }
+    msg
+  })
+}
--- dev2/app.R
+++ dev2/app.R
@@ -18,7 +18,7 @@ odin_fit_ui <- function(initial_code) {
       shiny::tabPanel(
         "Configure",
         icon = shiny::icon("random"),
-        odin_fit_configure_ui()),
+        mod_configure_ui("odin_configure")),
       shiny::tabPanel(
         "Visualise",
         icon = shiny::icon("search"),
@@ -30,38 +30,13 @@ odin_fit_ui <- function(initial_code) {
 }
 
 
-odin_fit_configure_ui <- function() {
-  shiny::tagList(
-    shiny::titlePanel("Configure"),
-    shiny::h3("Data"),
-    shiny::textOutput("data_status"),
-    shiny::selectInput("data_time_variable",
-                       "Select time variable",
-                       character(0)))
-}
-
-
 odin_fit_server <- function(initial_code) {
   function(input, output, session) {
     data <- shiny::callModule(mod_csv_server, "odin_csv")
-    editor <- shiny::callModule(
+    model <- shiny::callModule(
       mod_editor_simple_server, "odin_editor", initial_code)
-
-    rv <- shiny::reactiveValues()
-
-    output$data_status <- shiny::renderText({
-      if (is.null(data())) {
-        msg <- "Please upload data"
-      } else {
-        msg <- sprintf("%d rows of data have been uploaded", nrow(data()))
-        vars <- names(data())
-        prev <- input$data_time_variable
-        selected <- if (!is.null(prev) && prev %in% vars) prev else NULL
-        shiny::updateSelectInput(session, "data_time_variable",
-                                 choices = vars, selected = selected)
-      }
-      msg
-    })
+    configure <- shiny::callModule(
+      mod_configure_server, "odin_configure", data, model)
   }
 }
 
