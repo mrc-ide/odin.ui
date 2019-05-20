798217ea5d64f9bb98df1a7849715c842f397ed0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-05-20 16:45:37 +0100

    Start on configuration interface

--- dev2/app.R
+++ dev2/app.R
@@ -18,7 +18,7 @@ odin_fit_ui <- function(initial_code) {
       shiny::tabPanel(
         "Configure",
         icon = shiny::icon("random"),
-        shiny::h2("CONFIGURE")),
+        odin_fit_configure_ui()),
       shiny::tabPanel(
         "Visualise",
         icon = shiny::icon("search"),
@@ -30,11 +30,38 @@ odin_fit_ui <- function(initial_code) {
 }
 
 
+odin_fit_configure_ui <- function() {
+  shiny::tagList(
+    shiny::titlePanel("Configure"),
+    shiny::h3("Data"),
+    shiny::textOutput("data_status"),
+    shiny::selectInput("data_time_variable",
+                       "Select time variable",
+                       character(0)))
+}
+
+
 odin_fit_server <- function(initial_code) {
   function(input, output, session) {
     data <- shiny::callModule(mod_csv_server, "odin_csv")
     editor <- shiny::callModule(
       mod_editor_simple_server, "odin_editor", initial_code)
+
+    rv <- shiny::reactiveValues()
+
+    output$data_status <- shiny::renderText({
+      if (is.null(data())) {
+        msg <- "Please upload data"
+      } else {
+        msg <- sprintf("%d rows of data have been uploaded", nrow(data()))
+        vars <- names(data())
+        prev <- input$data_time_variable
+        selected <- if (!is.null(prev) && prev %in% vars) prev else NULL
+        shiny::updateSelectInput(session, "data_time_variable",
+                                 choices = vars, selected = selected)
+      }
+      msg
+    })
   }
 }
 
