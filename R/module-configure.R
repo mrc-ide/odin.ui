ef8c1f62bc17f908312a16354406547b71313035
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-05-21 11:29:38 +0100

    Start on configure interface

--- R/module-configure.R
+++ R/module-configure.R
@@ -3,16 +3,22 @@ mod_configure_ui <- function(id) {
   shiny::tagList(
     shiny::titlePanel("Configure"),
     shiny::h3("Data"),
-    shiny::textOutput(ns("data_status")),
+    shiny::textOutput(ns("data_summary")),
     shiny::selectInput(ns("data_time_variable"),
                        "Select time variable",
-                       character(0)))
+                       character(0)),
+    shiny::h3("Model"),
+    shiny::uiOutput(ns("model_status")),
+    shiny::textOutput(ns("model_summary")))
 }
 
 
 mod_configure_server <- function(input, output, session, data, model) {
-  output$data_status <- shiny::renderText({
+  output$data_summary <- shiny::renderText({
     if (is.null(data())) {
+      ## Ideally this would point us back to the data tab with a link
+      ## but that requires passing in the parent session:
+      ## https://stackoverflow.com/a/54751068
       msg <- "Please upload data"
     } else {
       msg <- sprintf("%d rows of data have been uploaded", nrow(data()))
@@ -27,10 +33,27 @@ mod_configure_server <- function(input, output, session, data, model) {
 
   shiny::observe({
     res <- model()
-    if (!is.null(res)) {
-      pars <- coef(res$generator)
+    status <- NULL
+
+    if (is.null(res)) {
+      msg <- "Please compile a model"
+    } else {
+      if (!res$is_current) {
+        status <- shiny::div(
+          class = "panel-group",
+          shiny::div(
+            class = "panel panel-warning",
+            shiny::div(class = "panel-heading",
+                       shiny::icon("exclamation fa-lg"),
+                       "Warning: model is out of date"),
+            shiny::div(class = "panel-body",
+                       "Consider recompiling the model")))
+      }
+
+      pars <- coef(res$result$model)
       msg <- sprintf("Model with %d parameters", ncol(pars))
-      output$model_status <- shiny::renderText(msg)
     }
+    output$model_status <- shiny::renderUI(status)
+    output$model_summary <- shiny::renderText(msg)
   })
 }
