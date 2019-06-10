38453f5c7b7736cddb118637f1379f327630ce58
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-06-10 17:05:43 +0100

    Refactor configure

--- R/module-configure.R
+++ R/module-configure.R
@@ -14,40 +14,26 @@ mod_configure_ui <- function(id) {
 
 
 mod_configure_server <- function(input, output, session, data, model) {
+  rv <- shiny::reactiveValues(link = NULL)
+
   output$data_summary <- shiny::renderText({
     configure_data_summary(data())
   })
 
-  rv <- shiny::reactiveValues(link = NULL)
+  output$model_summary <- shiny::renderText({
+    configure_model_summary(model())
+  })
 
-  shiny::observe({
-    res <- model()
-    status <- NULL
+  output$model_status <- shiny::renderUI({
+    configure_model_status(model())
+  })
 
-    if (is.null(res)) {
-      msg <- "Please compile a model"
-    } else {
-      if (!res$is_current) {
-        status <- shiny::div(
-          class = "panel-group",
-          shiny::div(
-            class = "panel panel-warning",
-            shiny::div(class = "panel-heading",
-                       shiny::icon("exclamation fa-lg"),
-                       "Warning: model is out of date"),
-            shiny::div(class = "panel-body",
-                       "Consider recompiling the model")))
-      }
-
-      pars <- coef(res$result$model)
-      msg <- sprintf("Model with %d parameters", ncol(pars))
-    }
-    output$model_status <- shiny::renderUI(status)
-    output$model_summary <- shiny::renderText(msg)
+  output$link_status <- shiny::renderText({
+    configure_link_status(rv$label)
   })
 
   output$link <- shiny::renderUI({
-    mod_configure_link_ui(input, session, rv, data, model, NULL)
+    configure_link_ui(session$ns, input, rv, data(), model(), NULL)
   })
 
   shiny::observe({
@@ -62,21 +48,13 @@ mod_configure_server <- function(input, output, session, data, model) {
     }
   })
 
-  output$link_status <- shiny::renderText({
-    if (length(rv$link) == 0L) {
-      "No linked variables"
-    } else {
-      paste(rv$label, collapse = " & ")
-    }
-  })
-
   get_state <- function() {
     list(link = rv$link)
   }
 
   set_state <- function(state) {
     output$link <- shiny::renderUI(
-      mod_configure_link_ui(input, session, rv, data, model, state$link))
+      configure_link_ui(session$ns, input, rv, data(), model(), state$link))
   }
 
   shiny::outputOptions(output, "link", suspendWhenHidden = FALSE)
@@ -90,52 +68,36 @@ mod_configure_server <- function(input, output, session, data, model) {
 }
 
 
-selected <- function(prev, choices) {
-  if (!is.null(prev) && prev %in% choices) prev else NA
-}
-
-
-mod_configure_link_ui <- function(input, session, rv, data, model, restore) {
-  d <- data()
-  m <- model()
-  if (!isTRUE(d$configured) || is.null(m)) {
+## This would be more testable split out a little further
+configure_link_ui <- function(ns, input, rv, data, model, restore) {
+  if (!isTRUE(data$configured) || is.null(model)) {
     rv$map <- NULL
   } else {
-    shiny::isolate({
-      if (is.null(restore)) {
-        prev <- lapply(rv$map, function(x) input[[x]])
-      } else {
-        prev <- restore
-      }
+    if (is.null(restore)) {
+      prev <- lapply(rv$map, function(x) input[[x]])
+    } else {
+      prev <- restore
+    }
 
-      vars_data <- d$name_vars
+    vars_data <- data$name_vars
+    vars_model <- model$result$info$vars$name
 
-      ## TODO: push this into the editor module so that we always have
-      ## this alongside the model as we use it in a couple of places
-      ## (see also fit.R's run_model_data)
-      metadata <- model_metadata(m$result$model)
-      vars_model <- c(
-        names(metadata$data$variable$contents),
-        names(metadata$data$output$contents))
+    ## Are any of these still current?
+    fmt <- "link_data_%s"
+    rv$map <- setNames(sprintf(fmt, vars_data), vars_data)
+    opts <- list(
+      placeholder = "Select variable",
+      onInitialize = I('function() { this.setValue(""); }'))
 
-      ## Are any of these still current?
-      fmt <- "link_data_%s"
-      rv$map <- setNames(sprintf(fmt, vars_data), vars_data)
-      opts <- list(
-        placeholder = "Select variable",
-        onInitialize = I('function() { this.setValue(""); }'))
+    selected <- set_names(rep(list(NULL), length(vars_data)), vars_data)
+    i <- names(prev) %in% vars_data &
+      unlist(prev, FALSE, FALSE) %in% vars_model
+    selected[names(prev)[i]] <- prev[i]
 
-      selected <- set_names(rep(list(NULL), length(vars_data)), vars_data)
-      i <- names(prev) %in% vars_data &
-        unlist(prev, FALSE, FALSE) %in% vars_model
-      selected[names(prev)[i]] <- prev[i]
-
-      ns <- session$ns
-      lapply(vars_data, function(x)
-        shiny::selectizeInput(
-          ns(sprintf(fmt, x)), x, selected = selected[[x]],
-          choices = vars_model, options = if (is.null(selected[[x]])) opts))
-    })
+    lapply(vars_data, function(x)
+      shiny::selectizeInput(
+        ns(sprintf(fmt, x)), x, selected = selected[[x]],
+        choices = vars_model, options = if (is.null(selected[[x]])) opts))
   }
 }
 
@@ -150,5 +112,33 @@ configure_data_summary <- function(data) {
     "Please select time variable for your data"
   } else {
     sprintf("%d rows of data have been uploaded", nrow(data$data))
+  }
+}
+
+
+configure_model_summary <- function(model) {
+  if (is.null(model)) {
+    "Please compile a model"
+  } else {
+    np <- nrow(model$result$info$pars)
+    nv <- nrow(model$result$info$vars)
+    sprintf("Model with %d parameters and %d variables/outputs", np, nv)
+  }
+}
+
+
+configure_model_status <- function(model) {
+  if (!is.null(model$is_current) && !model$is_current) {
+    simple_panel("warning", "Warning: model is out of date",
+                 "Consider recompiling the model")
+  }
+}
+
+
+configure_link_status <- function(label) {
+  if (length(label) == 0L) {
+    "No linked variables"
+  } else {
+    paste(label, collapse = " & ")
   }
 }
--- tests/testthat/test-module-configure.R
+++ tests/testthat/test-module-configure.R
@@ -9,3 +9,30 @@ test_that("data summary", {
                                            data = matrix(0, 3, 4))),
                "3 rows of data have been uploaded")
 })
+
+
+test_that("model summary", {
+  expect_equal(configure_model_summary(NULL),
+               "Please compile a model")
+
+  x <- list(result = list(info = list(pars = matrix(0, 3, 0),
+                                      vars = matrix(0, 4, 0))))
+  expect_equal(configure_model_summary(x),
+               "Model with 3 parameters and 4 variables/outputs")
+})
+
+
+test_that("model status", {
+  expect_null(configure_model_status(NULL))
+  expect_null(configure_model_status(list(is_current = TRUE)))
+  expect_equal(configure_model_status(list(is_current = FALSE)),
+               simple_panel("warning", "Warning: model is out of date",
+                            "Consider recompiling the model"))
+})
+
+
+test_that("link status", {
+  expect_equal(configure_link_status(character(0)), "No linked variables")
+  expect_equal(configure_link_status("a"), "a")
+  expect_equal(configure_link_status(c("a", "b")), "a & b")
+})
