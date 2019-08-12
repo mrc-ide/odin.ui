8aaae623f5124f1ffce85721206ccdd9c40a2efa
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-08-12 12:21:18 +0100

    Add some basic tests

--- tests/testthat/test-control-focal.R
+++ tests/testthat/test-control-focal.R
@@ -19,3 +19,24 @@ test_that("batch_focal", {
     control_focal_result(name, pct, n, user),
     list(base = user, name = "x", value = 1, n = 10, from = 0.9, to = 1.1))
 })
+
+
+test_that("pct_to_range", {
+  expect_equal(
+    control_focal_pct_to_range(10, 15),
+    list(from = 8.5, to = 11.5))
+})
+
+
+test_that("range_to_pct", {
+  expect_equal(
+    control_focal_range_to_pct(10, 8.5, 11.5), 15)
+})
+
+
+test_that("pct_to_range - empty cases", {
+  empty <- list(from = NA, to = NA)
+  expect_equal(control_focal_pct_to_range(NULL, NULL), empty)
+  expect_equal(control_focal_pct_to_range(NULL, 15), empty)
+  expect_equal(control_focal_pct_to_range(10, NULL), empty)
+})
