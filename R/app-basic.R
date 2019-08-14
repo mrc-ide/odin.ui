d53851ff4e0c50c59a88703501dfcf9d7994f6ba
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-08-14 10:00:12 +0100

    Rename elements more consistently

--- R/app-basic.R
+++ R/app-basic.R
@@ -13,15 +13,15 @@ odin_basic_ui <- function() {
       shiny::tabPanel(
         "Editor",
         icon = shiny::icon("edit"),
-        odin.ui:::mod_editor_simple_ui("odin_editor")),
+        odin.ui:::mod_editor_simple_ui("editor")),
       shiny::tabPanel(
         "Visualise",
         icon = shiny::icon("search"),
-        odin.ui:::mod_vis_ui("odin_vis")),
+        odin.ui:::mod_vis_ui("vis")),
       shiny::tabPanel(
         "Sensitivity",
         icon = shiny::icon("bars"),
-        odin.ui:::mod_batch_ui("odin_batch")),
+        odin.ui:::mod_batch_ui("batch")),
       shiny::tabPanel(
         shiny::tagList(
           "Load/Save",
@@ -34,16 +34,16 @@ odin_basic_ui <- function() {
 odin_basic_server <- function(initial_code) {
   function(input, output, session) {
     model <- shiny::callModule(
-      odin.ui:::mod_editor_simple_server, "odin_editor", initial_code,
+      odin.ui:::mod_editor_simple_server, "editor", initial_code,
       "Return to the Editor tab")
     data <- link <- shiny::reactive(NULL)
 
     run_options <- odin.ui:::control_run_options(control_end_time = TRUE)
     vis <- shiny::callModule(
-      odin.ui:::mod_vis_server, "odin_vis", data, model$result, link,
+      odin.ui:::mod_vis_server, "vis", data, model$result, link,
       run_options = run_options)
     batch <- shiny::callModule(
-      odin.ui:::mod_batch_server, "odin_batch", model$result, data, link,
+      odin.ui:::mod_batch_server, "batch", model$result, data, link,
       run_options = run_options)
 
     modules <- odin.ui:::submodules(model = model, vis = vis, batch = batch)
