929b1a8479ccda26e538b5350a7ea1b0262c27c0
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-08-15 16:43:48 +0100

    Strip namespace qualification off imported apps

--- R/app-basic.R
+++ R/app-basic.R
@@ -13,44 +13,44 @@ odin_basic_ui <- function() {
       shiny::tabPanel(
         "Editor",
         icon = shiny::icon("edit"),
-        odin.ui:::mod_editor_simple_ui("editor")),
+        mod_editor_simple_ui("editor")),
       shiny::tabPanel(
         "Visualise",
         icon = shiny::icon("search"),
-        odin.ui:::mod_vis_ui("vis")),
+        mod_vis_ui("vis")),
       shiny::tabPanel(
         "Sensitivity",
         icon = shiny::icon("bars"),
-        odin.ui:::mod_batch_ui("batch")),
+        mod_batch_ui("batch")),
       shiny::tabPanel(
         shiny::tagList(
           "Load/Save",
-          odin.ui:::mod_status_ui("status")),
+          mod_status_ui("status")),
         icon = shiny::icon("list"),
-        odin.ui:::mod_state_ui("state"))))
+        mod_state_ui("state"))))
 }
 
 
 odin_basic_server <- function(initial_code) {
   function(input, output, session) {
     model <- shiny::callModule(
-      odin.ui:::mod_editor_simple_server, "editor", initial_code,
+      mod_editor_simple_server, "editor", initial_code,
       "Return to the Editor tab")
     data <- link <- shiny::reactive(NULL)
 
-    run_options <- odin.ui:::control_run_options(control_end_time = TRUE)
+    run_options <- control_run_options(control_end_time = TRUE)
     vis <- shiny::callModule(
-      odin.ui:::mod_vis_server, "vis", data, model$result, link,
+      mod_vis_server, "vis", data, model$result, link,
       run_options = run_options)
     batch <- shiny::callModule(
-      odin.ui:::mod_batch_server, "batch", model$result, data, link,
+      mod_batch_server, "batch", model$result, data, link,
       run_options = run_options)
 
-    modules <- odin.ui:::submodules(model = model, vis = vis, batch = batch)
+    modules <- submodules(model = model, vis = vis, batch = batch)
     state <- shiny::callModule(
-      odin.ui:::mod_state_server, "state", modules, "prototype")
+      mod_state_server, "state", modules, "prototype")
     status <- shiny::callModule(
-      odin.ui:::mod_status_server, "status",
+      mod_status_server, "status",
       list(edit = model$result))
   }
 }
--- R/app-stochastic.R
+++ R/app-stochastic.R
@@ -8,7 +8,7 @@ odin_stochastic <- function(path) {
 odin_stochastic_ui <- function() {
   shiny::shinyUI(
     shiny::tagList(
-      odin.ui:::odin_css(),
+      odin_css(),
       shiny::navbarPage(
         "Stochasticity",
         id = "odin_ui_navvar",
@@ -16,15 +16,15 @@ odin_stochastic_ui <- function() {
         shiny::tabPanel(
           "Code",
           icon = shiny::icon("align-justify"),
-          odin.ui:::mod_model_static_ui("model")),
+          mod_model_static_ui("model")),
         shiny::tabPanel(
           "Visualise",
           icon = shiny::icon("search"),
-          odin.ui:::mod_vis_ui("vis")),
+          mod_vis_ui("vis")),
         shiny::tabPanel(
           "Sensitivity",
           icon = shiny::icon("bars"),
-          odin.ui:::mod_batch_ui("batch"))
+          mod_batch_ui("batch"))
       )))
 }
 
@@ -32,17 +32,17 @@ odin_stochastic_ui <- function() {
 odin_stochastic_server <- function(path) {
   function(input, output, session) {
     data <- link <- shiny::reactive(NULL)
-    run_options <- odin.ui:::control_run_options(
+    run_options <- control_run_options(
       control_end_time = TRUE, replicates = TRUE, scale_time = TRUE,
       default_end_time = 100, default_replicates = 5)
 
     model <- shiny::callModule(
-      odin.ui:::mod_model_static_server, "model", path, "Model")
+      mod_model_static_server, "model", path, "Model")
     vis <- shiny::callModule(
-      odin.ui:::mod_vis_server, "vis", data, model$result, link,
+      mod_vis_server, "vis", data, model$result, link,
       run_options = run_options)
     batch <- shiny::callModule(
-      odin.ui:::mod_batch_server, "batch", model$result, data, link,
+      mod_batch_server, "batch", model$result, data, link,
       run_options = run_options)
   }
 }
