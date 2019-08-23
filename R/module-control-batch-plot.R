e819094698fe3d44a9c912a0c5833b6abce53cba
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2019-08-23 10:14:58 +0100

    Remove misbehaving restore
    This is the last of the old restore

--- R/module-control-batch-plot.R
+++ R/module-control-batch-plot.R
@@ -21,36 +21,17 @@ mod_control_batch_plot_server <- function(input, output, session, render) {
                       extreme_type = input$extreme_type)
   })
 
-  get_state <- function() {
-    if (render()) {
-      list(type = input$type,
-           slice_time = input$slice_time,
-           extreme_type = input$extreme_type)
-    }
-  }
-
-  set_state <- function(state) {
-    if (!is.null(state)) {
-      output$ui <- shiny::renderUI(
-        control_batch_plot_ui(TRUE, session$ns, state))
-      output$options <- shiny::renderUI(
-        control_batch_plot_options(state$type, session$ns, state))
-    }
-  }
-
   reset <- function() {
     output$ui <- shiny::renderUI(
       control_batch_plot_ui(render(), session$ns))
   }
 
   list(result = shiny::reactive(rv$result),
-       get_state = get_state,
-       set_state = set_state,
        reset = reset)
 }
 
 
-control_batch_plot_ui <- function(render, ns, restore = NULL) {
+control_batch_plot_ui <- function(render, ns) {
   if (!isTRUE(render)) {
     return(NULL)
   }
@@ -62,23 +43,20 @@ control_batch_plot_ui <- function(render, ns, restore = NULL) {
 
   odin_control_section(
     "Plot options",
-    simple_select_input("Type of plot", ns("type"), types, restore$type),
+    simple_select_input("Type of plot", ns("type"), types),
     shiny::uiOutput(ns("options")),
     ns = ns)
 }
 
 
-control_batch_plot_options <- function(type, ns, restore = NULL) {
+control_batch_plot_options <- function(type, ns) {
   switch(
-    type,
+    type %||% "slice",
     slice = simple_numeric_input(
-      "Time to use (default is last)", ns("slice_time"),
-      restore$slice_time %||% NA),
+      "Time to use (default is last)", ns("slice_time"), NA),
     extreme = simple_select_input(
-      "Extreme to use", ns("extreme_type"), c("max", "min"),
-      restore$extreme_type),
+      "Extreme to use", ns("extreme_type"), c("max", "min")),
     textreme = simple_select_input(
-      "Extreme to use", ns("extreme_type"), c("max", "min"),
-      restore$extreme_type),
+      "Extreme to use", ns("extreme_type"), c("max", "min")),
     NULL)
 }
